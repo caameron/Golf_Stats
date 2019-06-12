5f0e79845facc6816f319999985feaecf95f07ea
caameron <30531788+caameron@users.noreply.github.com>
2019-06-11 19:27:19 -0700

    added in frontend react and backend springboot

---
+++ spring-boot-golf-stats/src/main/java/com/golf_stats/springbootgolfstats/SpringBootGolfStatsApplication.java
@@ -0,0 +1,13 @@
+package com.golf_stats.springbootgolfstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGolfStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGolfStatsApplication.class, args);
+	}
+
+}
